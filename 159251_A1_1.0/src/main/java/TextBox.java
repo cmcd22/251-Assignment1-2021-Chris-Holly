8e61c599a77b4c65eec58830c90d64a9675754d8
Holly <71049594+HDuCoing@users.noreply.github.com>
2021-08-05 14:23:15 +1200

    Add files via upload

---
+++ 159251_A1_1.0/src/main/java/TextBox.java
@@ -0,0 +1,178 @@
+import javax.swing.*;
+import javax.swing.plaf.metal.MetalLookAndFeel;
+import javax.swing.plaf.metal.OceanTheme;
+import javax.swing.text.DefaultEditorKit;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+public class TextBox extends JFrame implements ActionListener{
+    JFrame frame;
+    JTextArea textArea;
+    public static void main(String[] args){
+        TextBox tb = new TextBox();
+        tb.newWindow();
+    }
+    public void newWindow(){
+        // Basic frame
+        frame = new JFrame("Text Editor");
+        // Basic text writing zone
+        textArea = new JTextArea();
+        // Menu bar containing important buttons
+        /*
+New function: to create a new (fresh) window.
+Open function: to read other text files (just standard .txt files). This should allow users to navigate the file
+system to search for a file.
+The ability to read OpenDocument Text (.odt) files. This is part of the Open function.
+
+Save function: save text output into .txt file format. This should allow users to navigate the file system
+to save the file in a selected drive/location.
+
+Search: search for text within the screen (this will be tested based on a single word)
+
+Exit: to quit the program – close all windows.
+
+Select text, Copy, Paste and Cut (SCPC) capabilities.
+
+Time and Date (T&D): retrieve the current time and data from the OS and place it at the
+top of the page of the editor.
+
+About:  display the names of both team members and a brief message in a popup message box.
+
+Print function: allow your editor to print text by connecting it to the local printer in your machine (
+similar to any other text editor that you have used).
+
+harder: include a PDF conversation function to your editor so the file can be also saved
+into PDF format (for standard text files). Use an external library for this such as Apache PDFBox  or OpenPDF.
+         */
+        //Menubar
+        JMenuBar menuBar = new JMenuBar();
+        //Menu
+        JMenu menu = new JMenu("File");
+        //Menu items
+        JMenuItem newFile = new JMenuItem("New");
+        JMenuItem openFile = new JMenuItem("Open");
+        JMenuItem saveFile = new JMenuItem("Save");
+        JMenuItem searchText = new JMenuItem("Search");
+        JMenuItem savetoPDF = new JMenuItem("Save to PDF");
+        JMenuItem printFile = new JMenuItem("Print");
+
+        //Action listeners
+        newFile.addActionListener(this);
+        openFile.addActionListener(this);
+        saveFile.addActionListener(this);
+        printFile.addActionListener(this);
+        searchText.addActionListener(this);
+        savetoPDF.addActionListener(this);
+        // Adding menu items to menu
+        menu.add(newFile);
+        menu.add(openFile);
+        menu.add(saveFile);
+        menu.add(savetoPDF);
+        menu.add(printFile);
+        // Exit option
+        JMenuItem exit = new JMenuItem("Exit");
+        exit.addActionListener(this);
+        //Date and time
+
+        // Add everything to the menu bar
+        menuBar.add(menu);
+        menuBar.add(searchText);
+        menuBar.add(exit);
+        // Display window
+        frame.setJMenuBar(menuBar);
+        frame.add(textArea);
+        frame.setSize(500,500);
+        frame.show();
+    }
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String event = e.getActionCommand();
+        // Paste clipboard
+        if (event.equals("paste")){
+            textArea.paste();
+        }
+        // Copy to clipboard
+        else if (event.equals("copy")){
+            textArea.copy();
+        }
+        // Cuts text
+        else if (event.equals("cut")){
+            textArea.cut();
+        }
+        else if (event.equals("Open")) {
+            JFileChooser j = new JFileChooser("f:");
+            // simple OpenDialog function
+            int r = j.showOpenDialog(null);
+            // If a file is selected
+            if (r == JFileChooser.APPROVE_OPTION) {
+                // Set the label to the path of the selected directory
+                File fi = new File(j.getSelectedFile().getAbsolutePath());
+                try {
+                    String s1 = "",
+                            sl = "";
+                    // File reader
+                    FileReader fr = new FileReader(fi);
+                    // Buffered reader
+                    BufferedReader br = new BufferedReader(fr);
+                    // Initialize sl
+                    sl = br.readLine();
+                    // Take the input from the file
+                    while ((s1 = br.readLine()) != null) {
+                        sl = sl + "\n" + s1;
+                    }
+                    // Set the text
+                    textArea.setText(sl);
+                } catch (Exception evt) {
+                    JOptionPane.showMessageDialog(frame, evt.getMessage());
+                }
+            }
+            // If open is cancelled
+            else
+                JOptionPane.showMessageDialog(frame, "Open cancelled.");
+        }
+        else if (event.equals("Save")){
+            JFileChooser j = new JFileChooser("f:");
+            // Simple SaveDialog function
+            int r = j.showSaveDialog(null);
+            if (r == JFileChooser.APPROVE_OPTION) {
+                // Set directory
+                File fi = new File(j.getSelectedFile().getAbsolutePath());
+                // Writes the file
+                try {
+                    // Create a file writer
+                    FileWriter wr = new FileWriter(fi, false);
+                    // Create buffered writer
+                    BufferedWriter w = new BufferedWriter(wr);
+                    // Write
+                    w.write(textArea.getText());
+                    w.flush();
+                    w.close();
+                }
+                catch (Exception evt) {
+                    JOptionPane.showMessageDialog(frame, evt.getMessage());
+                }
+            }
+            // If save is cancelled
+            else
+                JOptionPane.showMessageDialog(frame, "Save cancelled.");
+
+        }
+        else if (event.equals("Print")) {
+            try {
+                // print the file
+                textArea.print();
+            }
+            catch (Exception evt) {
+                JOptionPane.showMessageDialog(frame, evt.getMessage());
+            }
+        }
+
+    }
+}
